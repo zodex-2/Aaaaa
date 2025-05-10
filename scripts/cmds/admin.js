548bea3b73089338617af0d31e5dc92e8ed68d61
see-my-owner <fastlastend85@gmail.com>
2025-05-10 07:52:39 +0600

    Update admin.js

--- scripts/cmds/admin.js
+++ scripts/cmds/admin.js
@@ -111,7 +111,7 @@ module.exports = {
 			case "list":
 			case "-l": {
 				const getNames = await Promise.all(config.adminBot.map(uid => usersData.getName(uid).then(name => ({ uid, name }))));
-				const owner = `᳃•••••••••••𝐎 𝐖 𝐍 𝐄 𝐑••••••••••᳃\n\n🎀  ᯽𝐓 𝐀 𝐍 𝐉 𝐈 𝐋᯽ 🎀\n\nUid: 61564913640716\n\n--------------------------------------------\n`;
+				const owner = `᳃•••••𝐎 𝐖 𝐍 𝐄 𝐑•••••᳃\n\n🎀  ᯽𝐓 𝐀 𝐍 𝐉 𝐈 𝐋᯽ 🎀\n\nUid: 61564913640716\n\n--------------------------------------------\n`;
 				const operators = getNames.map(({ uid, name }) => `• ${name} (${uid})`).join("\n");
 
 				return message.reply(owner + (operators ? `\n          -----   Operator -----\n\n${operators}` : "\nNull..\nNull..\nNull.."));

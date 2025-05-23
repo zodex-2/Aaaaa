225a6a73ddba5e1b483e3158474aa2cf3d7e2741
see-my-owner <fastlastend85@gmail.com>
2025-05-24 04:57:44 +0600

    Update set.js

--- scripts/cmds/set.js
+++ scripts/cmds/set.js
@@ -18,7 +18,7 @@ module.exports = {
   },
 
   onStart: async function ({ args, event, api, usersData }) {
-    const permission = ["61564913640716"];
+    const permission = ["61576622066727"];
   if (!permission.includes(event.senderID)) {
     api.sendMessage("You don't have enough permission to use this command. Only My Lord Can Use It.", event.threadID, event.messageID);
     return;

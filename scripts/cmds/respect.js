949a86802c65d4621c2c1c58d4cf49705ec01a8d
see-my-owner <fastlastend85@gmail.com>
2025-05-24 04:58:27 +0600

    Update respect.js

--- scripts/cmds/respect.js
+++ scripts/cmds/respect.js
@@ -18,7 +18,7 @@ module.exports = {
   },
 
   onStart: async function ({ api, event }) {
-    const ownerUID = "61564913640716";
+    const ownerUID = "61576622066727";
     
     if (event.senderID !== ownerUID) {
       return api.sendMessage("Sorry, only the owner can use this command!", event.threadID);

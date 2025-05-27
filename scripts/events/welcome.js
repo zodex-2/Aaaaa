cf5ce972155ef3b8fb8b4a33d868d9ffbe598078
see-my-owner <fastlastend85@gmail.com>
2025-05-28 05:50:14 +0600

    Update welcome.js

--- scripts/events/welcome.js
+++ scripts/events/welcome.js
@@ -26,10 +26,10 @@ module.exports = {
 			session2: "noon",
 			session3: "afternoon",
 			session4: "evening",
-			welcomeMessage: "Thank you for inviting me to the group!\nBot prefix: %1\nTo view the list of commands, please enter: %1help",
+			welcomeMessage: "successful in bed for group\nBot prefix: %1",
 			multiple1: "you",
 			multiple2: "you guys",
-			defaultWelcomeMessage: `Hello {userName}.\nWelcome {multiple} to the chat group: {boxName}\nHave a nice {session} 😊`
+			defaultWelcomeMessage: `hilu 🐥 {userName}. welcome to the {multiple}✨{boxName} ✨🐥`
 		}
 	},
 

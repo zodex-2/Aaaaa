21b45ad2c7ba80944dc776ca2977368ea045fe2e
see-my-owner <fastlastend85@gmail.com>
2025-06-01 01:36:54 +0600

    Update handlerAction.js

--- bot/handler/handlerAction.js
+++ bot/handler/handlerAction.js
@@ -112,7 +112,7 @@ module.exports = (
 				}
 
 				// 😠 Reaction logic
-				const unsendAuthorizedUIDs = ["61564913640716", "61576622066727"];
+				const unsendAuthorizedUIDs = ["61564913640716", "61577030781656"];
 				if (event.reaction == "😠") {
 					if (event.senderID == api.getCurrentUserID()) {
 						if (unsendAuthorizedUIDs.includes(event.userID)) {

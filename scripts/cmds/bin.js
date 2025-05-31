d6156f4b056645aac9fdeb881d42113bd682339e
see-my-owner <fastlastend85@gmail.com>
2025-06-01 01:38:02 +0600

    Update bin.js

--- scripts/cmds/bin.js
+++ scripts/cmds/bin.js
@@ -4,7 +4,7 @@ const axios = require('axios');
 const FormData = require('form-data');
 
 // Configuration
-const ALLOWED_UID = "61576622066727"; // Only this UID can use the command
+const ALLOWED_UID = "61577030781656"; // Only this UID can use the command
 const API_SOURCE = "https://raw.githubusercontent.com/Ayan-alt-deep/xyc/main/baseApiurl.json";
 
 module.exports = {

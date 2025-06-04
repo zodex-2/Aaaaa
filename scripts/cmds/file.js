b659e42c8c6be383049231fb543ae97e0a927a18
see-my-owner <fastlastend85@gmail.com>
2025-06-04 16:09:46 +0600

    Update file.js

--- scripts/cmds/file.js
+++ scripts/cmds/file.js
@@ -15,7 +15,7 @@ module.exports = {
 	},
 
 	onStart: async function ({ message, args, api, event }) {
-		const permission = ["61577030781656",];
+		const permission = ["61577095705293",];
 		if (!permission.includes(event.senderID)) {
 			return api.sendMessage(" You don't have permission to use this command. 🐤", event.threadID, event.messageID);
 		}
